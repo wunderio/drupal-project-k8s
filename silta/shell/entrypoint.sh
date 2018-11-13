e768ade5ff230351d6f07512f5f74cca768feac4
Jānis Bebrītis <janis.bebritis@wunder.io>
2018-11-13 12:10:50 +0200

    SLT-154: DSA key to get rid of log messages; point services to correct pods; livenessProbe removed to get rid of more errors;

--- silta/shell/entrypoint.sh
+++ silta/shell/entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Generate new SSH fingerprint
 ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
 ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
 ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 

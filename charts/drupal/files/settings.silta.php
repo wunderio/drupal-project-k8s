f99ea5f1c53f476b4d20dd6d7de8bc0882644271
Marc Galang <marc.galang@wunder.io>
2020-11-11 13:27:41 +0200

    SLT-505: Port the memcache settings changes from charts repository

--- charts/drupal/files/settings.silta.php
+++ charts/drupal/files/settings.silta.php
@@ -49,8 +49,13 @@ if ($elasticsearch_host = getenv('ELASTICSEARCH_HOST')) {
 if (getenv('MEMCACHED_HOST')) {
   $settings['memcache']['servers'] = [getenv('MEMCACHED_HOST') . ':11211' => 'default'];
 
-  // Set the memcache backend if class is defined.
-  if (class_exists('\Drupal\memcache\MemcacheBackend')) {
+  // Set the default cache backend to use memcache if memcache host is set
+  // and if one of the memcache libraries was found.
+  // Cache backends should not be set to memcache during installation.
+  // The existence of the memcache drupal module should also be checked
+  // but this is not possible until this issue has been fixed:
+  // https://www.drupal.org/project/drupal/issues/2766509
+  if (!\Drupal\Core\Installer\InstallerKernel::installationAttempted() && (class_exists('Memcache', FALSE) || class_exists('Memcached', FALSE))) {
     $settings['cache']['default'] = 'cache.backend.memcache';
   }
 }
